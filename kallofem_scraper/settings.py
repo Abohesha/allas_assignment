b8101e4bb8139491710130d96a4019d0730b96ec
abohesha <mohamedhesh646@gmail.com>
2025-04-30 22:04:52 +0200

    remove api

--- kallofem_scraper/settings.py
+++ kallofem_scraper/settings.py
@@ -3,6 +3,9 @@ BOT_NAME = "kallofem_scraper"
 SPIDER_MODULES = ["kallofem_scraper.spiders"]
 NEWSPIDER_MODULE = "kallofem_scraper.spiders"
 
-ROBOTSTXT_OBEY = True
+FEEDS = {
+    "output/products.json": {"format": "json", "overwrite": True},
+}
+ROBOTSTXT_OBEY = False
 
 FEED_EXPORT_ENCODING = "utf-8"
